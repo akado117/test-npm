7486466509688583044c6022cc6813e898e7a40e
Alex Kaidan <akaidan@Alexs-MacBook-Pro.local>
2019-12-30 16:28:33 -0500

    made shell script call from relative path

--- bin/create.sh
+++ bin/create.sh
@@ -1,12 +1,16 @@
 # this should only be ran via npm scripts
 
+parent_path="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
+
 create_package_json() {
-    node ./js/merge_and_create_package_json.js --directory $1
+    echo $parent_path
+
+    node $parent_path/js/merge_and_create_package_json.js --directory $1
 }
 
 for assets_dir in apps/*/assets; do
     echo "===> $assets_dir"
-    create_package_json $assets_dir
+    (create_package_json $assets_dir)
 done
 
 
